55cad416ddc9c21dd44f5cd559c3d9e5f2bdb486
Sathiyaseelan Velu <sathiyaseelan.velu@softsuave.com>
2024-03-06 21:22:10 +0530

    Feat: Enhanced extension by implementing prompt message functionality

--- src/content.tsx
+++ src/content.tsx
@@ -1,7 +1,8 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
+import { useEffect, useRef, useState } from "react"
 
-import { CountButton } from "~features/count-button"
+import IndexPopup from "./popup"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://*.linkedin.com/*"]
@@ -13,12 +14,97 @@ export const getStyle = () => {
   return style
 }
 
-const PlasmoOverlay = () => {
+const TextAreaOverlay = () => {
+  const textAreaContainerRef = useRef<HTMLElement | null>(null)
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+  const [showModal, setShowModal] = useState<boolean>(false)
+
+  const handleInsertMessage = (message: string) => {
+    if (!textAreaContainerRef.current) return
+    const textArea = textAreaContainerRef.current.querySelector("p")
+    if (textArea) {
+      textArea.innerText = message
+      textArea.dispatchEvent(new Event("input", { bubbles: true }))
+      textAreaContainerRef.current.focus()
+      setShowModal(false)
+    }
+  }
+
+  useEffect(() => {
+    const handleFocusEvent = (event: FocusEvent) => {
+      const target = event.target as HTMLElement
+      if (target && target.classList.contains("msg-form__contenteditable")) {
+        textAreaContainerRef.current = target
+        let buttonPosition = {
+          x:
+            target.getBoundingClientRect().left +
+            (target.clientWidth -
+              parseInt(window.getComputedStyle(target).marginRight)),
+          y: target.getBoundingClientRect().top + (target.clientHeight - 40)
+        }
+        if (buttonRef.current) {
+          const button = buttonRef.current
+          button.style.left = buttonPosition.x + "px"
+          button.style.top = buttonPosition.y + "px"
+          button.style.display = "block"
+        }
+      }
+    }
+    // Add a focus event listener to the document body
+    document.body.addEventListener("focus", handleFocusEvent, true)
+    return () => {
+      document.body.removeEventListener("focus", handleFocusEvent, true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!buttonRef.current) return
+    const img = buttonRef.current.querySelector("img")
+    if (showModal) {
+      img.src = chrome.runtime.getURL("assets/generating.svg")
+    } else {
+      img.src = chrome.runtime.getURL("assets/ai_icon.svg")
+    }
+  }, [showModal])
+
+  /* The `useEffect` hook in the provided code snippet is setting up an event listener for the `click`
+  event on the window. When a click event occurs outside of text area, it'll hide the extension icon */
+  useEffect(() => {
+    const handleClickEvent = (e: MouseEvent) => {
+      const target = e.target
+      if (
+        target instanceof HTMLElement &&
+        (target.closest(".msg-form__contenteditable") ||
+          target.tagName === "PLASMO-CSUI")
+      )
+        return
+      buttonRef.current.style.display = "none"
+      textAreaContainerRef.current = null
+    }
+
+    window.addEventListener("click", handleClickEvent)
+    return () => {
+      window.removeEventListener("click", handleClickEvent)
+    }
+  }, [])
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
   return (
-    <div className="z-50 flex fixed top-32 right-8">
-      <CountButton />
-    </div>
+    <>
+      <button
+        onClick={() => setShowModal(true)}
+        ref={buttonRef}
+        className="fixed hidden">
+        <img src={chrome.runtime.getURL("assets/ai_icon.svg")} alt="" />
+      </button>
+      {showModal && (
+        <IndexPopup onInsert={handleInsertMessage} onClose={handleClose} />
+      )}
+    </>
   )
 }
 
-export default PlasmoOverlay
+export default TextAreaOverlay
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,144 @@
-import { CountButton } from "~features/count-button"
+import { useRef, useState } from "react"
 
 import "~style.css"
 
-function IndexPopup() {
+const STATIC_MESSAGE =
+  "Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask."
+
+interface IProps {
+  onInsert: (text: string) => void
+  onClose: () => void
+}
+
+function IndexPopup({ onInsert, onClose }: IProps) {
+  const modalRef = useRef<HTMLDivElement | null>(null)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const promptsContainerRef = useRef<HTMLDivElement | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [showRegenerate, setShowRegenerate] = useState<boolean>(false)
+
+  const addPrompt = (prompt: string) => {
+    const promptsContainer = promptsContainerRef.current
+    const div = document.createElement("div")
+    div.className =
+      "max-w-[80%] p-4 rounded-xl bg-gray-100 text-2xl text-gray-500 self-end"
+    div.innerText = prompt
+    promptsContainer.appendChild(div)
+    promptsContainer.scrollTop = promptsContainer.scrollHeight
+  }
+
+  const addPromptResponse = (response: string) => {
+    const promptsContainer = promptsContainerRef.current
+    const div = document.createElement("div")
+    div.className =
+      "max-w-[80%] p-4 rounded-xl bg-[#DBEAFE] text-2xl text-gray-500 self-start"
+    div.innerText = response
+    promptsContainer.appendChild(div)
+    promptsContainer.scrollTop = promptsContainer.scrollHeight
+  }
+
+  const handleGenerate = () => {
+    const input = inputRef.current
+    if (!input.value) return
+    const prompt = input.value
+    addPrompt(prompt)
+    setShowRegenerate(true)
+    setLoading(true)
+    setTimeout(() => {
+      addPromptResponse(STATIC_MESSAGE)
+      setLoading(false)
+    }, 500)
+    input.value = ""
+  }
+
+  const handleRegenerate = () => {}
+
+  const handleInsertMessage = () => {
+    onInsert(STATIC_MESSAGE)
+  }
+
+  /**
+   * The handleClickOutside function checks if a click event occurred outside a specified modal element
+   * and calls the onClose function if it did.
+   */
+  const handleClickOutside = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (!modalRef.current.contains(e.target as Node)) {
+      onClose()
+    }
+  }
+
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
+    <div
+      className="fixed top-0 backdrop-contrast-[.6] h-screen w-screen flex items-center justify-center"
+      onClick={handleClickOutside}>
+      <div
+        ref={modalRef}
+        id="prompt_modal"
+        className="shadow-md bg-[#F9FAFB] min-h-[192px] w-[870px] rounded-2xl p-[26px] flex flex-col gap-[26px] items-end">
+        {/* Container for prompts */}
+        <div
+          className="w-full flex flex-col gap-[26px]"
+          ref={promptsContainerRef}
+        />
+
+        {/* Loading indicator */}
+        {loading && (
+          <div className="self-start">
+            <img
+              src={chrome.runtime.getURL("assets/loader.gif")}
+              alt=""
+              className="h-12"
+            />
+          </div>
+        )}
+
+        {/* Input for prompts */}
+        <input
+          className="flex justify-center items-center gap-4 w-[818px] h-[61px] shadow-inner rounded-xl border-2 text-2xl font-medium text-gray-500 placeholder:text-gray-300 px-4 py-1.5 focus:outline-[#3B82F6]"
+          placeholder="Your Prompt"
+          autoFocus
+          ref={inputRef}
+        />
+
+        {/* Buttons for actions */}
+        {showRegenerate ? (
+          <div className="flex gap-[26px]">
+            <button
+              className="btn outlined"
+              onClick={handleInsertMessage}
+              type="button">
+              <img
+                src={chrome.runtime.getURL("assets/arrow_down.svg")}
+                alt=""
+                className="h-6 w-6"
+              />
+              <label>Insert</label>
+            </button>
+            <button
+              className="btn filled"
+              onClick={handleRegenerate}
+              type="button">
+              <img
+                src={chrome.runtime.getURL("assets/refresh.svg")}
+                alt=""
+                className="h-6 w-6"
+              />
+              <label>Regenerate</label>
+            </button>
+          </div>
+        ) : (
+          <button className="btn filled" onClick={handleGenerate} type="button">
+            <img
+              src={chrome.runtime.getURL("assets/send.svg")}
+              alt=""
+              className="h-6 w-6"
+            />
+            <label>Generate</label>
+          </button>
+        )}
+      </div>
     </div>
   )
 }
